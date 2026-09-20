3c2690aca2c9c4a203380be764e48570f4678305
WilsonCoder786 <muhammad.zubair@jumppace.com>
2026-09-20 16:03:11 +0500

    update

--- user Site/signUp.js
+++ user Site/signUp.js
@@ -1,84 +1,107 @@
-var signup = document.getElementById("signup")
-var email = document.getElementById("email")
-var password = document.getElementById("password")
-var username = document.getElementById("name")
-
+var signup = document.getElementById("signup");
+var email = document.getElementById("email");
+var password = document.getElementById("password");
+var username = document.getElementById("name");
 
 signup.addEventListener("click", async function () {
-    console.log(email.value, password.value)
+  console.log(email.value, password.value);
 
-    await firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
-        .then(async (result) => {
-            console.log(result.user.uid) // database id 
-            if (result.user.uid) {
-                var imageUrl = await uploadimage()
+  var courseKey =
+    courseList.options[courseList.selectedIndex].getAttribute("key");
+  console.log(courseKey);
+  console.log(courseList.value);
 
-                await firebase.database().ref("user").child(result.user.uid).set({
-                    email: email.value,
-                    password: password.value,
-                    name: username.value,
-                    image : imageUrl,
-                    createdAt: firebase.database.ServerValue.TIMESTAMP
-                })
-                alert("SignUp Successfully")
-                setTimeout(() => {
-                    // window.location.href="login.html"
-                    window.location.replace("login.html")
-                }, 2000)
+  await firebase
+    .auth()
+    .createUserWithEmailAndPassword(email.value, password.value)
+    .then(async (result) => {
+      console.log(result.user.uid); // database id
+      if (result.user.uid) {
+        var imageUrl = await uploadimage();
 
-            }
-
-        })
-        .catch((error) => {
-            alert(error)
-
-        })
-
-
-})
-
+        await firebase.database().ref("user").child(result.user.uid).set({
+          email: email.value,
+          password: password.value,
+          name: username.value,
+          image: imageUrl,
+          createdAt: firebase.database.ServerValue.TIMESTAMP,
+          courseName: courseList.value,
+          courseKey: courseKey,
+        });
+        alert("SignUp Successfully");
+        setTimeout(() => {
+          // window.location.href="login.html"
+          window.location.replace("login.html");
+        }, 2000);
+      }
+    })
+    .catch((error) => {
+      alert(error);
+    });
+});
 
 async function uploadimage() {
-    const fileInput = document.getElementById("imageUpload")
-    const file = fileInput.files[0]
+  const fileInput = document.getElementById("imageUpload");
+  const file = fileInput.files[0];
 
-    var imageUrl =""
-    if (!file) {
-        return alert("please select profile image")
-    }
-    const cloudName = "zmpw6oge"
-    const uploadFolder = "upload"
-    const formData = new FormData()
-    formData.append("file", file)
-    formData.append("upload_preset", uploadFolder)
-    // var loading = document.getElementById("loading")
-    // var btn = document.getElementById("btn")
-    // btn.style.display="none"
-    // loading.style.display="inline"
+  var imageUrl = "";
+  if (!file) {
+    return alert("please select profile image");
+  }
+  const cloudName = "sgnp0trm";
+  const uploadFolder = "storageQuizApp";
+  const formData = new FormData();
+  formData.append("file", file);
+  formData.append("upload_preset", uploadFolder);
+  // var loading = document.getElementById("loading")
+  // var btn = document.getElementById("btn")
+  // btn.style.display="none"
+  // loading.style.display="inline"
 
-    try {
-        const response = await fetch(`https://api.cloudinary.com/v1_1/${cloudName}/image/upload`, {
-            method: "POST",
-            body: formData
-        })
+  try {
+    const response = await fetch(
+      `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
+      {
+        method: "POST",
+        body: formData,
+      },
+    );
 
-        const data = await response.json()
+    const data = await response.json();
 
-        console.log(data)
-        alert("profile imaeg upload")
-        // window.location.href=data.secure_url
-        imageUrl = data.secure_url
-          return imageUrl
-
-
-
-
-    }
-    catch (e) {
-        alert("error", e)
-    }
-
-  
-
-
+    console.log(data);
+    alert("profile imaeg upload");
+    // window.location.href=data.secure_url
+    imageUrl = data.secure_url;
+    return imageUrl;
+  } catch (e) {
+    alert("error", e);
+  }
 }
+
+///////////////////////////////////
+
+let getAlLCourses = async () => {
+  await firebase
+    .database()
+    .ref("course")
+    .get()
+    .then((db) => {
+      console.log(db.val()); //firebase data base human read form convert
+      var arrList = Object.values(db.val()); //object to array
+      console.log(arrList);
+      for (var value of arrList) {
+        console.log(value);
+        courseList.innerHTML += `
+            <option key=${value.coursekey}>${value.courseName}</option>
+            `;
+      }
+      courseList.style.display = "inline";
+    })
+
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+getAlLCourses();
